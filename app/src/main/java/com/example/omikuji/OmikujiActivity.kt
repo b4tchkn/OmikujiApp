50433afdabdcfdda771ca0126c8f4c24e9afe78c
batch <baaaakkrad@gmail.com>
2019-07-02 10:21:32 +0900

    [update]random fortune

--- app/src/main/java/com/example/omikuji/OmikujiActivity.kt
+++ app/src/main/java/com/example/omikuji/OmikujiActivity.kt
@@ -2,11 +2,26 @@ package com.example.omikuji
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.main.*
+import java.util.*
 
 class OmikujiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        var str = "大吉"
+        val rnd = Random()
+        val number = rnd.nextInt(3)
+        val omikujiShelf = Array<String>(20, {"吉"})
+
+
+        when (number) {
+            0 -> str = "吉"
+            1 -> str = "凶"
+        }
+
+        fortuneTxt.text = str
     }
 }
