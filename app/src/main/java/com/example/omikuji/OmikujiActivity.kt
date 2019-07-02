a7d404e6312f9dbae7ccd171e9cc068c355fdcf2
batch <baaaakkrad@gmail.com>
2019-07-02 09:00:51 +0900

    [update]rename file name

--- app/src/main/java/com/example/omikuji/OmikujiActivity.kt
+++ app/src/main/java/com/example/omikuji/OmikujiActivity.kt
@@ -3,10 +3,10 @@ package com.example.omikuji
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OmikujiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
     }
 }
